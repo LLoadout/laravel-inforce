e1ef403fb9fe6f33df712d79e5329fa944c5e5fd
Dieter Coopman <dieter@deltasolutions.be>
2021-08-31 18:41:49 +0200

    add description for role in seeder

--- database/seeders/InforceSeeder.php
+++ database/seeders/InforceSeeder.php
@@ -21,124 +21,125 @@ class InforceSeeder extends Seeder
     {
         app()->make(\Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
 
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::table('permissions')->truncate();
-        DB::table('users')->truncate();
-        DB::table('menus')->truncate();
-        DB::table('model_has_permissions')->truncate();
-        DB::table('role_has_permissions')->truncate();
-        DB::table('roles')->truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        DB::statement("SET FOREIGN_KEY_CHECKS=0;");
+        DB::table("permissions")->truncate();
+        DB::table("users")->truncate();
+        DB::table("menus")->truncate();
+        DB::table("model_has_permissions")->truncate();
+        DB::table("role_has_permissions")->truncate();
+        DB::table("roles")->truncate();
+        DB::statement("SET FOREIGN_KEY_CHECKS=1;");
 
-        collect(['admin', 'user'])->each(function ($name) {
-            DB::table('roles')->insert([
-                'name'       => $name,
-                'guard_name' => 'web'
+        collect(["admin", "user"])->each(function ($name) {
+            DB::table("roles")->insert([
+                "name"        => $name,
+                "description" => "an $name in your application",
+                "guard_name"  => "web"
             ]);
         });
 
         $sortOrder = 999;
 
         $usersMenu = Menu::create([
-            'name'       => 'User management',
-            'permission' => 'menu.user-management',
-            'sort_order' => $sortOrder++,
-            'editable'   => false
+            "name"       => "User management",
+            "permission" => "menu.user-management",
+            "sort_order" => $sortOrder++,
+            "editable"   => false
         ]);
 
         Permission::create([
-            'name'       => 'menu.user-management',
-            'guard_name' => 'web',
-            'editable'   => false
+            "name"       => "menu.user-management",
+            "guard_name" => "web",
+            "editable"   => false
         ]);
 
         Menu::create([
-            'parent_id'  => $usersMenu->id,
-            'name'       => 'Manage users',
-            'route'      => 'users.index',
-            'permission' => 'menu.user-management.manage-users',
-            'sort_order' => $sortOrder++,
-            'editable'   => false
+            "parent_id"  => $usersMenu->id,
+            "name"       => "Manage users",
+            "route"      => "users.index",
+            "permission" => "menu.user-management.manage-users",
+            "sort_order" => $sortOrder++,
+            "editable"   => false
         ]);
 
         Permission::create([
-            'name'       => 'menu.user-management.manage-users',
-            'guard_name' => 'web',
-            'editable'   => false
+            "name"       => "menu.user-management.manage-users",
+            "guard_name" => "web",
+            "editable"   => false
         ]);
 
         Menu::create([
-            'parent_id'  => $usersMenu->id,
-            'name'       => 'Manage roles',
-            'route'      => 'users.roles',
-            'permission' => 'menu.user-management.manage-roles',
-            'sort_order' => $sortOrder++,
-            'editable'   => false
+            "parent_id"  => $usersMenu->id,
+            "name"       => "Manage roles",
+            "route"      => "users.roles",
+            "permission" => "menu.user-management.manage-roles",
+            "sort_order" => $sortOrder++,
+            "editable"   => false
         ]);
 
         Permission::create([
-            'name'       => 'menu.user-management.manage-roles',
-            'guard_name' => 'web',
-            'editable'   => false
+            "name"       => "menu.user-management.manage-roles",
+            "guard_name" => "web",
+            "editable"   => false
         ]);
 
         $devMenu = Menu::create([
-            'name'       => 'Developer menu',
-            'permission' => 'menu.developer-menu',
-            'sort_order' => $sortOrder++,
-            'editable'   => false
+            "name"       => "Developer menu",
+            "permission" => "menu.developer-menu",
+            "sort_order" => $sortOrder++,
+            "editable"   => false
         ]);
 
         Permission::create([
-            'name'       => 'menu.developer-menu',
-            'guard_name' => 'web',
-            'editable'   => false
+            "name"       => "menu.developer-menu",
+            "guard_name" => "web",
+            "editable"   => false
         ]);
 
         Menu::create([
-            'parent_id'  => $devMenu->id,
-            'name'       => 'Permissions',
-            'route'      => 'developers.permissions',
-            'permission' => 'menu.developer-menu.permissions',
-            'sort_order' => $sortOrder++,
-            'editable'   => false
+            "parent_id"  => $devMenu->id,
+            "name"       => "Permissions",
+            "route"      => "developers.permissions",
+            "permission" => "menu.developer-menu.permissions",
+            "sort_order" => $sortOrder++,
+            "editable"   => false
         ]);
 
         Permission::create([
-            'name'       => 'menu.developer-menu.permissions',
-            'guard_name' => 'web',
-            'editable'   => false
+            "name"       => "menu.developer-menu.permissions",
+            "guard_name" => "web",
+            "editable"   => false
         ]);
 
         Menu::create([
-            'parent_id'  => $devMenu->id,
-            'name'       => 'Menus',
-            'route'      => 'developers.menus',
-            'permission' => 'menu.developer-menu.menus',
-            'sort_order' => $sortOrder++,
-            'editable'   => false
+            "parent_id"  => $devMenu->id,
+            "name"       => "Menus",
+            "route"      => "developers.menus",
+            "permission" => "menu.developer-menu.menus",
+            "sort_order" => $sortOrder++,
+            "editable"   => false
         ]);
 
         Permission::create([
-            'name'       => 'menu.developer-menu.menus',
-            'guard_name' => 'web',
-            'editable'   => false
+            "name"       => "menu.developer-menu.menus",
+            "guard_name" => "web",
+            "editable"   => false
         ]);
 
-        $developer = Role::create(['name' => 'superuser']);
+        $developer = Role::create(["name" => "superuser", "description" => "the super user of your application"]);
 
-        $developer->givePermissionTo('menu.user-management');
-        $developer->givePermissionTo('menu.user-management.manage-users');
-        $developer->givePermissionTo('menu.user-management.manage-roles');
+        $developer->givePermissionTo("menu.user-management");
+        $developer->givePermissionTo("menu.user-management.manage-users");
+        $developer->givePermissionTo("menu.user-management.manage-roles");
 
-        $developer->givePermissionTo('menu.developer-menu');
-        $developer->givePermissionTo('menu.developer-menu.permissions');
-        $developer->givePermissionTo('menu.developer-menu.menus');
+        $developer->givePermissionTo("menu.developer-menu");
+        $developer->givePermissionTo("menu.developer-menu.permissions");
+        $developer->givePermissionTo("menu.developer-menu.menus");
 
-        $user = User::create(['name' => 'John Doe', 'email' => 'john@doe.com', 'password' => \Hash::make('password')]);
+        $user = User::create(["name" => "John Doe", "email" => "john@doe.com", "password" => \Hash::make("password")]);
 
         User::all()->each(function ($user) {
-            $user->assignRole('superuser');
+            $user->assignRole("superuser");
         });
 
 
