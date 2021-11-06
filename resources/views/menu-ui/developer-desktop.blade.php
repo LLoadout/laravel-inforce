c689320a2c4e872c34e73be33f722d8c5eff8112
Suhendra <sayasuhendra@gmail.com>
2021-11-06 23:59:11 +0700

    added livewire component for developer menus ui.

---
+++ resources/views/menu-ui/developer-desktop.blade.php
@@ -0,0 +1,24 @@
+<div class="ml-3 relative">
+	<x-jet-dropdown align="right" width="48">
+	    <x-slot name="trigger">
+		<span class="inline-flex rounded-md">
+		    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
+			{{ __('Developer Menu') }}
+			<svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+			    <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+			</svg>
+		    </button>
+		</span>
+	    </x-slot>
+    
+	    <x-slot name="content">
+		<x-jet-dropdown-link href="{{ route('developers.permissions') }}">
+		    {{ __('Permissions') }}
+		</x-jet-dropdown-link>
+		<x-jet-dropdown-link href="{{ route('developers.menus') }}">
+		    {{ __('Menus') }}
+		</x-jet-dropdown-link>
+	    </x-slot>
+	</x-jet-dropdown>
+    </div>
+    
