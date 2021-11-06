d0266b5cfbb0eb6f90303f8235e3d3e65c1cc3b2
Suhendra <sayasuhendra@gmail.com>
2021-11-06 23:57:05 +0700

    added livewire component for user management menu ui.

---
+++ src/Http/Livewire/UserManagementMenus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LLoadoutInforce\Http\Livewire;
+
+use Livewire\Component;
+
+class UserManagementMenus extends Component
+{
+    public function render()
+    {
+        return view('LLoadoutInforce-views::menu-ui.user-desktop');
+    }
+}
